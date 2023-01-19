cd7d1bda5a2947b5486cdb907639ec7ce884d002
Matis <93186421+MatisBevilacqua@users.noreply.github.com>
2023-01-19 13:12:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-let s = 4 /* cette variable va définir le nombre de ligne à chaque étage, elle s'acrémente de 1 à chaque étage*/
+let s = 4;
+let v = ''; 
+let c = 0 ;
 
-let v = '' /* cette variable contiendra les étoiles, pour former le sapin*/
-
-let c = 0 /* cette variable sert à content le nombre d'étage, afin de l'utiliser pour défenir le nombre d'étoiles en fonction des étoiles. */
-
-const QUESTION = 3 /* cette variable définie le nombre d'étages */
+const QUESTION = 3;
 
 
 for(let nbrEtage = 0; nbrEtage < QUESTION; nbrEtage++){
-    s += 1
+    s += 1;
     c++;
     for(let nbrline = 1; nbrline < s; nbrline++){
-        console.log(v += '*')
-        v += '*'
+        console.log(v += '*');
+        v += '*';
     }
-    v = '*'.repeat(c * 2)
+    v = '*'.repeat(c * 2);
 }
 
